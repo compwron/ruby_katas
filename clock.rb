f7be676eb81e65c177da8d2dfb5cd1c377df5c83
Linda Goldstein <lgoldste@thoughtworks.com>
2012-03-14 09:07:50 -0700

    Cleaning up the whitespace war between sublime and mate. Added readme. ;-}

--- clock.rb
+++ clock.rb
@@ -4,42 +4,42 @@ class Clock
   YELLOW = "Y"
   OFF    = "O"
   
-	def initialize(time_string)
-	  hours_minutes_seconds = time_string.split(":")
-	  print_seconds(hours_minutes_seconds[2].to_i)
-	  print_hours(hours_minutes_seconds[0].to_i)
-	  print_minutes(hours_minutes_seconds[1].to_i)
-	end
-	
-	private
-	def print_seconds(time)
-	  puts (time % 2 == 0) ? YELLOW : OFF
-	end
-	
-	def print_hours(time)
-	  print_four_lamps(time / 5, RED)
-	  print_four_lamps(time % 5, RED)
-	end
-	
-	def print_minutes(time)
-	  print_eleven_lamps(time / 5)
-	  print_four_lamps(time % 5, YELLOW)
-	end
+  def initialize(time_string)
+    hours_minutes_seconds = time_string.split(":")
+    print_seconds(hours_minutes_seconds[2].to_i)
+    print_hours(hours_minutes_seconds[0].to_i)
+    print_minutes(hours_minutes_seconds[1].to_i)
+  end
+  
+  private
+  def print_seconds(time)
+    puts (time % 2 == 0) ? YELLOW : OFF
+  end
+  
+  def print_hours(time)
+    print_four_lamps(time / 5, RED)
+    print_four_lamps(time % 5, RED)
+  end
+  
+  def print_minutes(time)
+    print_eleven_lamps(time / 5)
+    print_four_lamps(time % 5, YELLOW)
+  end
 
-	def print_four_lamps(num_lit, color)
-		puts on_lamps(num_lit, color) + off_lamps(4 - num_lit)
-	end
-	
-	def print_eleven_lamps(num_lit)
-		lit_lamps = 1.upto(num_lit).map { |lamp| (lamp % 3 == 0) ? RED : YELLOW }.join('')
-		puts lit_lamps + off_lamps(11 - lit_lamps.length)
-	end
-	
-	def on_lamps(num, color)
-	  color * num
-	end
-	
-	def off_lamps(num)
-	  OFF * num
-	end
+  def print_four_lamps(num_lit, color)
+    puts on_lamps(num_lit, color) + off_lamps(4 - num_lit)
+  end
+  
+  def print_eleven_lamps(num_lit)
+    lit_lamps = 1.upto(num_lit).map { |lamp| (lamp % 3 == 0) ? RED : YELLOW }.join('')
+    puts lit_lamps + off_lamps(11 - lit_lamps.length)
+  end
+  
+  def on_lamps(num, color)
+    color * num
+  end
+  
+  def off_lamps(num)
+    OFF * num
+  end
 end
--- spec/clock_spec.rb
+++ spec/clock_spec.rb
@@ -1,85 +1,94 @@
 require_relative "../clock.rb"
 
 describe Clock do
-	context "yellow light" do
-		it "is on, every other second" do
+  context "yellow light" do
+    it "is on, every other second" do
       assert_row_equals("Y")
       assert_row_equals("OOOO")
       assert_row_equals("OOOO")
-		  assert_row_equals("OOOOOOOOOOO")
-		  assert_row_equals("OOOO")
-			Clock.new("00:00:00")
-		end
-		
-		it "is off on odd-numbered seconds" do
-		  assert_row_equals("O")
-		  assert_row_equals("OOOO")
-		  assert_row_equals("OOOO")
-			assert_row_equals("OOOOOOOOOOO")
-			assert_row_equals("OOOO")
-			Clock.new("00:00:01")
-		end
-	end
+      assert_row_equals("OOOOOOOOOOO")
+      assert_row_equals("OOOO")
+      Clock.new("00:00:00")
+    end
 
-	context "hour lamps" do
-		it "shows four lamps on row 2 for 10am" do
-		  assert_row_equals("Y")
-		  assert_row_equals("RROO")
-		  assert_row_equals("OOOO")
-		  assert_row_equals("OOOOOOOOOOO")
-		  assert_row_equals("OOOO")
-			Clock.new("10:00:00")
-		end
-		
-		it "shows lamps on both rows for 11am" do
-		  assert_row_equals("Y")
-		  assert_row_equals("RROO")
-		  assert_row_equals("ROOO")
-		  assert_row_equals("OOOOOOOOOOO")
-		  assert_row_equals("OOOO")
-			Clock.new("11:00:00")
-		end
+    it "is off on odd-numbered seconds" do
+      assert_row_equals("O")
+      assert_row_equals("OOOO")
+      assert_row_equals("OOOO")
+      assert_row_equals("OOOOOOOOOOO")
+      assert_row_equals("OOOO")
+      Clock.new("00:00:01")
+    end
+  end
 
-		it "shows lamps on both rows for 2pm" do
-		  assert_row_equals("Y")
-		  assert_row_equals("RROO")
-		  assert_row_equals("RRRR")
-		  assert_row_equals("OOOOOOOOOOO")
-		  assert_row_equals("OOOO")
-			Clock.new("14:00:00")
-		end
-	end
+  context "hour lamps" do
+    it "shows four lamps on row 2 for 10am" do
+      assert_row_equals("Y")
+      assert_row_equals("RROO")
+      assert_row_equals("OOOO")
+      assert_row_equals("OOOOOOOOOOO")
+      assert_row_equals("OOOO")
+      Clock.new("10:00:00")
+    end
 
-	context "minute lamps" do
-		it "shows two yellow minute lamps for ten minutes past the hour" do
-			assert_row_equals("Y")
-		  assert_row_equals("OOOO")
-		  assert_row_equals("OOOO")
-			assert_row_equals("YYOOOOOOOOO")
-			assert_row_equals("OOOO")
-			Clock.new("00:10:00")
-		end
-		
-		it "shows red lamps for minutes on the quarter-hour" do
-		  assert_row_equals("Y")
-		  assert_row_equals("OOOO")
-		  assert_row_equals("OOOO")
-			assert_row_equals("YYROOOOOOOO")
-			assert_row_equals("OOOO")
-			Clock.new("00:15:00")
-		end
+    it "shows lamps on both rows for 11am" do
+      assert_row_equals("Y")
+      assert_row_equals("RROO")
+      assert_row_equals("ROOO")
+      assert_row_equals("OOOOOOOOOOO")
+      assert_row_equals("OOOO")
+      Clock.new("11:00:00")
+    end
 
-		it "shows yellow lamps for minutes not divisible by five" do
-		  assert_row_equals("Y")
-		  assert_row_equals("OOOO")
-		  assert_row_equals("OOOO")
-			assert_row_equals("YYROOOOOOOO")
-			assert_row_equals("YYYO")
-			Clock.new("00:18:00")
-		end
-	end
+    it "shows lamps on both rows for 2pm" do
+      assert_row_equals("Y")
+      assert_row_equals("RROO")
+      assert_row_equals("RRRR")
+      assert_row_equals("OOOOOOOOOOO")
+      assert_row_equals("OOOO")
+      Clock.new("14:00:00")
+    end
+  end
 
-	def assert_row_equals(string)
-		$stdout.should_receive(:puts).with(string)
-	end
+  context "minute lamps" do
+    it "shows two yellow minute lamps for ten minutes past the hour" do
+      assert_row_equals("Y")
+      assert_row_equals("OOOO")
+      assert_row_equals("OOOO")
+      assert_row_equals("YYOOOOOOOOO")
+      assert_row_equals("OOOO")
+      Clock.new("00:10:00")
+    end
+
+    it "shows red lamps for minutes on the quarter-hour" do
+      assert_row_equals("Y")
+      assert_row_equals("OOOO")
+      assert_row_equals("OOOO")
+      assert_row_equals("YYROOOOOOOO")
+      assert_row_equals("OOOO")
+      Clock.new("00:15:00")
+    end
+
+    it "shows yellow lamps for minutes not divisible by five" do
+      assert_row_equals("Y")
+      assert_row_equals("OOOO")
+      assert_row_equals("OOOO")
+      assert_row_equals("YYROOOOOOOO")
+      assert_row_equals("YYYO")
+      Clock.new("00:18:00")
+    end
+
+    it "shows time paired" do
+      assert_row_equals("O")
+      assert_row_equals("OOOO")
+      assert_row_equals("RROO")
+      assert_row_equals("YYRYYROOOOO")
+      assert_row_equals("OOOO")
+      Clock.new("02:30:01")
+    end
+  end
+
+  def assert_row_equals(string)
+    $stdout.should_receive(:puts).with(string)
+  end
 end
