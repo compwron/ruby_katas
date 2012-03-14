9c4412efadb20e6e9ae6a5964148251d9aee8e05
Rebecca Sliter <rsliter@thoughtworks.com>
2012-03-13 22:13:28 -0500

    testing for seconds\!

--- clock.rb
+++ clock.rb
@@ -1,3 +1,14 @@
 class Clock
+  attr_accessor :hours_minutes_seconds
+  
+	def initialize(time_string)
+	  self.hours_minutes_seconds = time_string.split(":")
+	  puts seconds
+	end
+	
+	private
+	def seconds
+	  "Y" if self.hours_minutes_seconds[2].to_i % 2 == 0
+	end
 	
 end
--- spec/clock_spec.rb
+++ spec/clock_spec.rb
@@ -3,7 +3,13 @@ require_relative "../clock.rb"
 describe Clock do
 	context "yellow light" do
 		it "is on, every other second" do
-			assert_equal "Y", Clock.new "00:00:00"
+      $stdout.should_receive(:puts).with("Y")
+			c = Clock.new("00:00:00")
+		end
+		
+		it "is off on odd-numbered seconds" do
+		  $stdout.should_receive(:puts).with("O")
+			c = Clock.new("00:00:01")
 		end
 	end
 end
