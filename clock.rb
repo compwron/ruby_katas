58e3909f1507c24edb6cf9befdb5f8483eac223c
Rebecca Sliter <rsliter@thoughtworks.com>
2012-03-13 22:45:02 -0500

    testing for second row on hours :)

--- clock.rb
+++ clock.rb
@@ -1,13 +1,17 @@
 class Clock
-  attr_accessor :hours_minutes_seconds
-  
 	def initialize(time_string)
-	  self.hours_minutes_seconds = time_string.split(":")
-	  puts seconds
+	  hours_minutes_seconds = time_string.split(":")
+	  print_seconds(hours_minutes_seconds[2].to_i)
+	  print_hours(hours_minutes_seconds[0].to_i)
 	end
 	
 	private
-	def seconds
-	  (self.hours_minutes_seconds[2].to_i % 2 == 0) ? "Y" : "O"
+	def print_seconds(time)
+	  puts (time % 2 == 0) ? "Y" : "O"
+	end
+	
+	def print_hours(time)
+	  r = time / 5
+	  puts "R" * (r) + "O" * (4 - r)
 	end
 end
--- spec/clock_spec.rb
+++ spec/clock_spec.rb
@@ -4,19 +4,29 @@ describe Clock do
 	context "yellow light" do
 		it "is on, every other second" do
       $stdout.should_receive(:puts).with("Y")
+      $stdout.should_receive(:puts).with("OOOO")
 			c = Clock.new("00:00:00")
 		end
 		
 		it "is off on odd-numbered seconds" do
 		  $stdout.should_receive(:puts).with("O")
+		  $stdout.should_receive(:puts).with("OOOO")
 			c = Clock.new("00:00:01")
 		end
 	end
 
 	context "hour lamps" do
 		it "shows four lamps on row 2 for 10am" do
-		  $stdout.should_receive(:puts).with("Y\nRROO")
-			c = Clock.new("00:10:00")
+		  $stdout.should_receive(:puts).with("Y")
+		  $stdout.should_receive(:puts).with("RROO")
+			c = Clock.new("10:00:00")
+		end
+		
+		it "shows lamps on both rows for 11am" do
+		  $stdout.should_receive(:puts).with("Y")
+		  $stdout.should_receive(:puts).with("RROO")
+		  $stdout.should_receive(:puts).with("ROOO")
+			c = Clock.new("11:00:00")
 		end
 	end
 end
