c47b96920431e2b43febb57dc798067b7b47a492
Linda Goldstein <lgoldste@thoughtworks.com>
2012-03-31 11:14:54 -0700

    WIP utter mess roman numerals

--- roman_numerals/spec/roman_numerals_spec.rb
+++ roman_numerals/spec/roman_numerals_spec.rb
@@ -5,6 +5,15 @@ describe RomanNumeral do
   	@rn = RomanNumeral.new
 	end
 
+  it "The Is come after the V when" do
+    @rn.ends_between_five_and_nine_exclusive(4).should == false
+    @rn.ends_between_five_and_nine_exclusive(5).should == false
+    @rn.ends_between_five_and_nine_exclusive(6).should == true
+    @rn.ends_between_five_and_nine_exclusive(7).should == true
+    @rn.ends_between_five_and_nine_exclusive(8).should == true
+    @rn.ends_between_five_and_nine_exclusive(9).should == false
+  end
+
   it "when given 1" do
   	@rn.roman(1).should == 'I'
   end
@@ -21,11 +30,40 @@ describe RomanNumeral do
     @rn.roman(4).should == 'IV'
   end
 
-  # it "when given 1" do
-  #   @rn.roman(1).should == 'I'
+  it "when given 5" do
+    @rn.roman(5).should == 'V'
+  end
+
+  it "when given 6" do
+    @rn.roman(6).should == 'VI'
+  end
+
+  it "when given 7" do
+    @rn.roman(7).should == 'VII'
+  end
+
+  it "when given 8" do
+    @rn.roman(8).should == 'VIII'
+  end
+
+  it "when given 9" do
+    @rn.roman(9).should == 'IX'
+  end
+
+  # it "when given 10" do
+  #   @rn.roman(10).should == 'X'
   # end
 
-  # it "when given 1" do
-  #   @rn.roman(1).should == 'I'
+  # it "when given 50" do
+  #   @rn.roman(50).should == 'L'
   # end
+
+  # it "when given 500" do
+  #   @rn.roman(500).should == 'D'
+  # end
+
+  # it "when given 1000" do
+  #   @rn.roman(1000).should == 'M'
+  # end
+
 end
