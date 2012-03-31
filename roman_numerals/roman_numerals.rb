9a8069914cd193c233fdd46e83ac45ccb134d7a0
Linda Goldstein <lgoldste@thoughtworks.com>
2012-03-31 16:29:21 -0700

    inelegant series of ifs. doing the test cases starting from the highest numbers.

--- roman_numerals/roman_numerals.rb
+++ roman_numerals/roman_numerals.rb
@@ -1,60 +1,20 @@
 class RomanNumeral
-  # def roman num
-  #   (num % 5 == 4) ? "I#{'V' * (num/5 + 1)}" : (num % 5 == 0) ? 'V' * (num/5) 
-  #   : (num % 5 == 1) ? ('V' * (num/5) + ('I' * (num % 5))) : 'error'
-  # end
-
-  # private
-
   def roman num
-    if ends_in_4? num 
-      result_for_ends_in_4 num 
-    # elsif divisible_by_5? num 
-    #   result_for_divisible_by_5
-    # end
-    else
-      result_for_ends_between_five_and_nine_exclusive num
+    output = ''
+    i = num
+    if i / 1000 > 0
+      output += ('M' * (i / 1000))
+      i = i - (1000 * (i / 1000))
     end
+    if i / 500 > 0
+      output += ('D' * (i / 500))
+      i = i - (500 * (i / 500))
+    end
+    if i / 50 > 0
+      output += ('L' * (i / 50))
+      i = i - (50 * (i / 50))
+    end
+    output
   end
-
-  def roman2 num
-    ('X' * (num/10)) + ('V' * (num/5)) + ('I' * (num % 5))
-  end
-
-  def ends_between_five_and_nine_exclusive num
-    remainder = (num % 5)
-    in_range = (0 < remainder && remainder < 4)
-    in_range
-  end
-
-  def result_for_ends_between_five_and_nine_exclusive num
-    'V' * (num/5) + ('I' * (num % 5))
-  end
-
-  private
-
-  def one_less_than_five num
-    num % 5 == 4
-  end
-
-  def ends_in_4? num
-    num % 5 == 4
-  end
-
-  def result_for_ends_in_4 num
-    "I#{'V' * (num/5 + 1)}"
-  end
-
-  def divisible_by_5? num
-    num % 5 == 0
-  end
-
-  def result_for_divisible_by_5 num # will change later
-    'V' * (num/5)
-  end
-
-
-
-
 end
 
