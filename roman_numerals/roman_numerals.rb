2542043fbea7866922af645dac3e7da3e3933714
Linda Goldstein <lgoldste@thoughtworks.com>
2012-04-02 13:15:54 -0500

    renaming now that there is no lookahead vs nonlookahead distinction

--- roman_numerals/roman_numerals.rb
+++ roman_numerals/roman_numerals.rb
@@ -1,7 +1,7 @@
 class RomanNumeral
   VALUE_MAPPER = {'M' => 1000, 'D' => 500, 'L' => 50, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1}
   
-  def non_lookahead_convert letter, letter_value
+  def convert letter, letter_value
     @@output += letter * (@@i / letter_value)
     @@i = @@i - (letter_value * (@@i  / letter_value))
   end
@@ -11,7 +11,7 @@ class RomanNumeral
     @@i = num
 
     VALUE_MAPPER.each {|letter, letter_value| 
-      non_lookahead_convert letter, letter_value
+      convert letter, letter_value
     }
     @@output
   end
