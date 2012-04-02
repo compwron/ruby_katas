bcfeae1ef056f06644c10a60902ae3d8489a2d0f
Linda Goldstein <lgoldste@thoughtworks.com>
2012-04-02 13:17:55 -0500

    removing unused class variables

--- roman_numerals/roman_numerals.rb
+++ roman_numerals/roman_numerals.rb
@@ -2,17 +2,17 @@ class RomanNumeral
   VALUE_MAPPER = {'M' => 1000, 'D' => 500, 'L' => 50, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1}
   
   def convert letter, letter_value
-    @@output += letter * (@@i / letter_value)
-    @@i = @@i - (letter_value * (@@i  / letter_value))
+    @output += letter * (@i / letter_value)
+    @i = @i - (letter_value * (@i  / letter_value))
   end
 
   def roman num
-    @@output = ''
-    @@i = num
+    @output = ''
+    @i = num
 
     VALUE_MAPPER.each {|letter, letter_value| 
       convert letter, letter_value
     }
-    @@output
+    @output
   end
 end
