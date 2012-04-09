61174fa99a34ea99ebde269a986265062d5694c8
Linda Goldstein <lgoldste@thoughtworks.com>
2012-04-09 13:25:53 -0500

    Refactoring but there are still to many magic numbers

--- red_pencil_promotion/sale_item.rb
+++ red_pencil_promotion/sale_item.rb
@@ -1,7 +1,4 @@
 class SaleItem
-  def initialize
-  end
-
   STABLE = 30
 
   def stable? price_history
@@ -13,23 +10,17 @@ class SaleItem
   end
 
   def valid_decrease? price_history
-  	if price_history.last <= price_history[-2] * 0.95 && price_history.last >= price_history[-2] * 0.70
-  		true
-  	else
-  		false
-  	end
+  	(price_history.last <= price_history[-2] * 0.95 && price_history.last >= price_history[-2] * 0.70) ? true : false
   end
 
   def valid_red_sale_start? price_history
   	price_history_today = price_history
-  	price_history_yesterday = price_history[0..price_history.length - 2]
+  	price_history_yesterday = price_history[0..price_history.count - 2]
   	valid_decrease?(price_history_today) && stable?(price_history_yesterday)
-
   end 
 
   def red_sale_ends_today? price_history
-  	pre_sale_price_history = price_history[0..price_history.length - 1 - STABLE]
+  	pre_sale_price_history = price_history[0..price_history.count - 1 - STABLE]
   	stable?(price_history) && stable?(pre_sale_price_history)
   end	
-
 end
