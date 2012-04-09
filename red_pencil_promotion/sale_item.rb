96da3845b67b24e9f1bbfbaf3ba446047a50e916
Linda Goldstein <lgoldste@thoughtworks.com>
2012-04-09 14:59:28 -0500

    WIP finding most recent stable price

--- red_pencil_promotion/sale_item.rb
+++ red_pencil_promotion/sale_item.rb
@@ -1,5 +1,7 @@
 class SaleItem
   STABLE = 30
+  TOP_SALE_PRICE_PERCENTAGE = 0.95
+  BOTTOM_SALE_PRICE_PERCENTAGE = 0.70
 
   def stable? price_history
   	most_recent_price = price_history.last
@@ -10,7 +12,7 @@ class SaleItem
   end
 
   def valid_decrease? price_history
-  	(price_history.last <= price_history[-2] * 0.95 && price_history.last >= price_history[-2] * 0.70) ? true : false
+  	(price_history.last <= price_history[-2] * TOP_SALE_PRICE_PERCENTAGE && price_history.last >= price_history[-2] * BOTTOM_SALE_PRICE_PERCENTAGE) ? true : false
   end
 
   def valid_red_sale_start? price_history
@@ -21,11 +23,45 @@ class SaleItem
 
   def red_sale_ends_today? price_history
   	return true unless sale_ends_if_price_increases(price_history) == false
-  	pre_sale_price_history = price_history[0..price_history.count - 1 - STABLE]
-  	stable?(pre_sale_price_history) 
+  	# return true unless price_decreases_below_threshhold(price_history) == false
+  	sale_has_lasted_max_length(price_history)
   end	
 
   def sale_ends_if_price_increases price_history
   	price_history.last > price_history[-2] ? true : false
+  end
+
+  def find_price_before_sale price_history
+		threshhold = get_most_recent_stable_price price_history
+		threshhold_value = price_history[threshhold]
+		
+		lowest_valid_sale_price = BOTTOM_SALE_PRICE_PERCENTAGE * threshhold
+
+		if valid_red_sale_start?(price_history[threshhold + 1])
+			(threshhold + 1).upto(price_history.last).each {|i|
+				return true if price_history[i] >= lowest_valid_sale_price
+			}
+		# else 
+			# false
+		end
+		# find the original increase
+		# - by, starting at the 30th value, checking for stability and incrementing until it is found
+		# calculate the threshhold
+		# for each value after the threshhold, check if value is above the threshhold
+
+  end
+
+  def get_most_recent_stable_price price_history
+  	threshhold = 0
+  	length_of_history = price_history.length - 1
+		(STABLE - 1).upto(length_of_history).each {|i|
+			if stable?(price_history[0..i]) then return price_history[i] end
+		}
+		0
+  end
+
+  def sale_has_lasted_max_length price_history
+  	pre_sale_price_history = price_history[0..price_history.count - 1 - STABLE]
+  	stable?(pre_sale_price_history) 
   end
 end
--- red_pencil_promotion/spec/sale_item_spec.rb
+++ red_pencil_promotion/spec/sale_item_spec.rb
@@ -4,9 +4,6 @@ describe SaleItem do
 
   subject { SaleItem.new }
 
-	before do
-	end
-
   it "price is stable for 30 days" do
   	subject.stable?([2] + [1] * 30).should == true
   	subject.stable?([2] + [1] * 30 + [3]).should == false
@@ -27,20 +24,33 @@ describe SaleItem do
   	subject.valid_red_sale_start?([100] * 28 + [90]).should == false
   end
 
-  it "red sale ends on time" do
-  	subject.red_sale_ends_today?([100] * 31 + [90] * 30).should == true
-  	subject.red_sale_ends_today?([100] * 31 + [90] * 31).should == false
-  end
+  context "red sale" do
+	  it "ends on time" do
+	  	subject.red_sale_ends_today?([100] * 31 + [90] * 30).should == true
+	  	subject.red_sale_ends_today?([100] * 31 + [90] * 31).should == false
+	  end
 
-  it "red sale is not prolonged by reduction during sale" do
-  	subject.red_sale_ends_today?([100] * 30 + [90] * 29 + [80]).should == true
-  	# subject.valid_red_sale_start?([100] * 30 + [90] * 30 + [80]).should == false
-  end
+	  it "is not prolonged by reduction during sale" do
+	  	subject.red_sale_ends_today?([100] * 30 + [90] * 29 + [80]).should == true
+	  	# subject.valid_red_sale_start?([100] * 30 + [90] * 30 + [80]).should == false
+	  end
 
-  it "red sale is ended immediately by price increase" do
-  	subject.red_sale_ends_today?([100] * 30 + [90] * 25 + [91]).should == true
-  	subject.red_sale_ends_today?([100] * 30 + [90] * 28 + [89]).should == false
-  	subject.red_sale_ends_today?([100] * 30 + [90] * 29 + [89]).should == true
-  end
+	  it "is ended immediately by price increase" do
+	  	subject.red_sale_ends_today?([100] * 30 + [90] * 25 + [91]).should == true
+	  	subject.red_sale_ends_today?([100] * 30 + [90] * 28 + [89]).should == false
+	  	subject.red_sale_ends_today?([100] * 30 + [90] * 29 + [89]).should == true
+	  end
+
+	  it "is ended immediately by price decrease to below 30% of original price" do
+	  	subject.get_most_recent_stable_price([100] * 30 + [90]).should == 100
+	  	subject.get_most_recent_stable_price([101] + [100] * 30 + [90]).should == 100
+	  	# subject.get_most_recent_stable_price([101] * 30 + [100] * 30 + [90]).should == 100
+
+	  	# subject.find_price_before_sale([100] * 30 + [90] * 25 + [89]).should == 100
+	  	# subject.price_decreases_below_threshhold([100] * 30 + [90] * 25 + [89]).should == false
+
+	  	# subject.price_decreases_below_threshhold([100] * 30 + [90] * 25 + [69]).should == true
+	  end
+	end
 end
 
