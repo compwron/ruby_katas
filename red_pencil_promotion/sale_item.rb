fe90be5d968f7fd6d93fc5be45ede81928c2ab6f
Linda Goldstein <lgoldste@thoughtworks.com>
2012-04-09 11:10:33 -0500

    redoing

--- red_pencil_promotion/sale_item.rb
+++ red_pencil_promotion/sale_item.rb
@@ -1,10 +1,36 @@
 class SaleItem
   def initialize
-    @price_history = []
   end
 
   def stable? price_history
-    true
+  	stable = true
+  	if price_history.length <= 30
+	    price_history.length.downto(price_history.length - 30) { |x|
+	    	if price_history[x + 1] && price_history[x + 1] == price_history[x]
+	    		stable
+	    	else 
+	    		stable = false
+	    	end
+	    }
+  	end
+  	stable
   end
+
+  def valid_decrease? price_history
+  	if price_history.last <= price_history[-2] * 0.95 && price_history.last >= price_history[-2] * 0.70
+  		true
+  	else
+  		false
+  	end
+  end
+
+  def valid_red_sale_start? price_history
+  	stable?(price_history) && valid_decrease?(price_history)
+  end 
+
+  def red_sale_ends_today? price_history
+  	pre_sale_price_history = price_history[0..price_history.length-30]
+  	stable?(price_history) && stable?(pre_sale_price_history)
+  end	
 
 end
--- red_pencil_promotion/spec/sale_item_spec.rb
+++ red_pencil_promotion/spec/sale_item_spec.rb
@@ -7,9 +7,27 @@ describe SaleItem do
 	before do
 	end
 
-  it "hi" do
-    price_history = nil
-    subject.stable?(price_history).should == true
+  it "price is stable for 30 days" do
+  	subject.stable?([1] * 31).should == true
+  	subject.stable?([1] * 28).should == false
+  end
+
+  it "sale-valid price change is between 5 and 30% downwards" do
+  	subject.valid_decrease?([100] * 31 + [90]).should == true
+  	subject.valid_decrease?([100] * 31 + [99]).should == false
+  	subject.valid_decrease?([100] * 31 + [50]).should == false
+  end
+
+  it "valid red sale start" do
+  	subject.valid_red_sale_start?([100] * 31 + [90]).should == true
+  	subject.valid_red_sale_start?([100] * 30 + [70]).should == true
+  	subject.valid_red_sale_start?([100] * 31 + [50]).should == false
+  	subject.valid_red_sale_start?([100] * 28 + [90]).should == false
+  end
+
+  it "red sale ends on time" do
+  	subject.red_sale_ends_today?([100] * 31 + [90] * 30).should == true
+  	# subject.red_sale_ends_today?([100] * 31 + [90] * 31).should == false
   end
 end
 
