438ff70d5211cc54953c2a30ffbece686230a3de
Linda Goldstein <lgoldste@thoughtworks.com>
2012-04-12 11:29:56 -0500

    Passing up to 8 => [2, 2, 2]

--- prime_factors/primes.rb
+++ prime_factors/primes.rb
@@ -7,7 +7,7 @@ class Primes
 
   def prime_factors num
   	factors(num).select {|factor| factor.prime?}.map {|prime_factor|
-      num / prime_factor == prime_factor ? [prime_factor, prime_factor] : prime_factor
+      (num / prime_factor) % prime_factor == 0 ? repeated_factors(num, prime_factor) : prime_factor
     }.flatten
   end
 
@@ -15,5 +15,15 @@ class Primes
   	1.upto(num).select {|i|
   		num % i == 0
   	}
+  end
+
+  def repeated_factors(num, factor, factors=[factor])
+    if (num / factor) == 1
+      return factors
+    else 
+      factors << factor
+      repeated_factors(num / factor, factor, factors)
+    end
+    factors
   end
 end
