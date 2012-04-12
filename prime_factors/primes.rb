1de9ce972f8d41d374f70a58d00cf91be5ebcd37
Linda Goldstein <lgoldste@thoughtworks.com>
2012-04-12 11:14:14 -0500

    Solved for 4 => [2, 2]

--- prime_factors/primes.rb
+++ prime_factors/primes.rb
@@ -2,6 +2,18 @@ require 'mathn'
 
 class Primes
   def primes num
-  	num ==1 ? [] : num.prime? ? [num] : [2, 2]
+  	num ==1 ? [] : num.prime? ? [num] : prime_factors(num)
+  end
+
+  def prime_factors num
+  	factors(num).select {|factor| factor.prime?}.map {|prime_factor|
+      num / prime_factor == prime_factor ? [prime_factor, prime_factor] : prime_factor
+    }.flatten
+  end
+
+  def factors num
+  	1.upto(num).select {|i|
+  		num % i == 0
+  	}
   end
 end
