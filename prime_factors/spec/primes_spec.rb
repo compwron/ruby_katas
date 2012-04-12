64f29d59541defd70248a4c60d03bb6fc0a56552
Linda Goldstein <lgoldste@thoughtworks.com>
2012-04-12 11:30:30 -0500

    Same solution works for 9 => [3, 3]

--- prime_factors/spec/primes_spec.rb
+++ prime_factors/spec/primes_spec.rb
@@ -5,7 +5,7 @@ describe Primes do
   subject { Primes.new }
 
   it "primes" do
-    prime_factors = {1 => [], 2 => [2], 3 => [3], 4 => [2, 2], 5 => [5], 6 => [2, 3], 7 => [7], 8 => [2, 2, 2]} 
+    prime_factors = {1 => [], 2 => [2], 3 => [3], 4 => [2, 2], 5 => [5], 6 => [2, 3], 7 => [7], 8 => [2, 2, 2], 9 => [3, 3]} 
     1.upto(prime_factors.length).each do |i|
       subject.primes(i).should == prime_factors[i]
     end
