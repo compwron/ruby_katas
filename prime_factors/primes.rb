387b41917533e61122401bffd7a608ffb638734f
Linda Goldstein <lgoldste@thoughtworks.com>
2012-04-12 11:32:48 -0500

    Fixing crappy whitespace in primes

--- prime_factors/primes.rb
+++ prime_factors/primes.rb
@@ -2,19 +2,19 @@ require 'mathn'
 
 class Primes
   def primes num
-  	num ==1 ? [] : num.prime? ? [num] : prime_factors(num)
+    num ==1 ? [] : num.prime? ? [num] : prime_factors(num)
   end
 
   def prime_factors num
-  	factors(num).select {|factor| factor.prime?}.map {|prime_factor|
+    factors(num).select {|factor| factor.prime?}.map {|prime_factor|
       (num / prime_factor) % prime_factor == 0 ? repeated_factors(num, prime_factor) : prime_factor
     }.flatten
   end
 
   def factors num
-  	1.upto(num).select {|i|
-  		num % i == 0
-  	}
+    1.upto(num).select {|i|
+      num % i == 0
+    }
   end
 
   def repeated_factors(num, factor, factors=[factor])
