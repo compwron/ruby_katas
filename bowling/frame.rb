34d4bf55c71540c522c10f1640e55062b96577e3
compwron <wordymusic@gmail.com>
2013-06-08 12:45:46 -0500

    frame knows it is over after a strike on first roll but not after a non-strike

--- bowling/frame.rb
+++ bowling/frame.rb
@@ -8,4 +8,8 @@ class Frame
 	def is_strike?
 		@rolls.first.pins_knocked_down == 10
 	end
+
+	def is_over?
+		is_strike? ? true : false # this is not a full implementation yet
+	end
 end
--- bowling/spec/frame_spec.rb
+++ bowling/spec/frame_spec.rb
@@ -12,6 +12,22 @@ describe Frame do
 
   	frame.is_strike?.should be_true
   end
+
+  it "ends frame after strike" do
+  	strike = Roll.new(1, 10) # roll 1, 10 pins down
+  	frame_number = 1
+  	frame = Frame.new(frame_number, strike)
+
+  	frame.is_over?.should == true
+  end
+
+  it "does not end frame after strike" do
+  	roll = Roll.new(1, 5) # roll 1, 5 pins down
+  	frame_number = 1
+  	frame = Frame.new(frame_number, roll)
+
+  	frame.is_over?.should == false
+  end
 end
 
 
